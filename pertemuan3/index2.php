23af07c6f2105170bd5e5bd47b51a3c9c19454cd
Glenn <121842204+GlenAguh@users.noreply.github.com>
2023-01-04 19:13:24 +0800

    Menambahkan Pertemuan 3

---
+++ pertemuan3/index2.php
@@ -0,0 +1,20 @@
+<?php 
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+
+ $i = 20;
+ if ($i < 10){
+    echo "benarrrr";
+ } else if ($i == 20 ){
+    echo "bingo";
+ }
+ else {
+    echo "salah";
+ }
+
+
+?>
